52f03ada7571100cb9cd64743c3ddce027c0d00c
mohideen-openwave <mohideen.openwave@gmail.com>
2013-09-04 14:58:09 +0530

    Revert "Revert "Revert "Beta 1.0"""
    This reverts commit 7f1ca9155140655e65d25e1f26c3f66ffcde5ad2.

--- mobile/urls.py
+++
@@ -1,8 +0,0 @@
-# oppia/mobile/urls.py
-from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
-
-
-urlpatterns = patterns('',
-    url(r'^scorecard/$', 'oppia.mobile.views.scorecard_view', name="oppia_mobile_scorecard"),
-)
